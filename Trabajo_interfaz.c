efc8395d46b5cc2f7075caa03c0bd0a68a87b789
davidbernaldo <61050055+davidbernaldo@users.noreply.github.com>
2020-03-18 18:15:17 +0100

    Add files via upload
    Primeros pasos en el desarrollo de la interfaz de usuario principal.

---
+++ Trabajo_interfaz.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+	char nombre[11];
+	int i = 0;
+
+	printf("Bienvenido, por favor, escribe tu nombre: ");
+	scanf("%s", nombre);
+	
+	printf("Hola, %s", nombre);
+	
+	
+}
