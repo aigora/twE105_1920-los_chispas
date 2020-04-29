3bb00ea35fcf6c06f914cc2b851f8c838c454332
javierchaz234 <j.chazarrab@aumnos.upm.es>
2020-04-29 18:35:11 +0200

    Añadidas funciones
    casi vacias

--- trabajo/Juego ahorcado/Ahorcado.c
+++ trabajo/Juego ahorcado/Ahorcado.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
+#include <string.h>
 #define N 10
 #define D 30
 
+//ESTRUCTURAS
 typedef struct{
 	char nomb[D];
 }adivinanza;
 
+//FUNCIONES 
+int numaleatorio(int n);//1
+void juego(char adivinapalabra[D]);//2
+int ahorcado(char adivinapalabra[E], char cadena[E]);//3
+int imprime_ahorcado(int intentos);//4
+
+//CODIGO PRINCIPAL
 void main()
 {
 
@@ -21,12 +31,12 @@ void main()
 		system ("PAUSE");
 			system("CLS");
 	printf("Bien, comencemos!!!!\n");
-	printf("Selecciona categoria:\na)Peliculas\nb)Series\nc)Equipos Futbol\n");
+	printf("Selecciona categoria:\n\ta)Peliculas\n\tb)Series\n\tc)Equipos Futbol\n");
    
     adivinanza peliculas[8]={"MISION IMPOSIBLE", "TOP GUN", "STAR WARS", "INDIANA JONES", "JUMANJI", "LOS VENGADORES", "EL IRLANDES","HARRY POTTER"};
     adivinanza series[8]={"JUEGO DE TRONOS", "BREAKING BAD", "LA CASA DE PAPEL", "ELITE", "ARROW", "POKEMON", "VIKINGOS", "STRANGER THINGS"};
 	adivinanza teams[8]={"REAL MADRID", "ATLETICO DE MADRID", "BARCELONA", "LIVERPOOL", "WATFORD", "GENK", "RIVER", "BBOCA JUNIORS"};
-    switch (letra)
+	switch (letra)
     {
        		case 'a':
 			case 'A':
@@ -34,7 +44,7 @@ void main()
 				system("CLS");
 				printf("Has elegido la categoria de Peliculas\n");
 				aleatorio = numaleatorio(8);
-				principal(peliculas[aleatorio].nomb, letrausuario);
+				juego(peliculas[aleatorio].nomb);
 	  			break;
 	           }
        		case 'b':
@@ -43,7 +53,7 @@ void main()
 				system("CLS");
 				printf("Has elegido la categoria de Series\n");
 				aleatorio = numaleatorio(8);
-				principal(series[aleatorio].nomb, letrausuario);
+				juego(series[aleatorio].nomb);
 	  			break;
 	           }
        		case 'c':
@@ -52,12 +62,54 @@ void main()
 				system("CLS");
 				printf("Has elegido la categoria de Equipos de Futbol\n");
 				aleatorio = numaleatorio(8);
-				principal(teams[aleatorio].nomb, letrausuario);
+				juego(teams[aleatorio].nomb);
 	  			break;
 	           }
+
+			default:
+			printf("Error, seleccione opcion valida\n");
+			system ("PAUSE");   
 			   			   			
 	}
-
-	system ("PAUSE");
-	system("CLS");
+    	printf("\n Quieres jugar de nuevo? Pulsa 1:");
+		if(getch()=='1') {
+			system("cls");
+		goto empezar;
+    }
 }
+
+
+
+//FUNCIONES 
+//1
+int numaleatorio(int n)
+{
+srand(time(NULL));
+int i;
+	i = rand()%n;
+	return i;	
+}
+//2
+
+void juego(char adivinapalabra[D])
+{int errores, intentos;
+while(1)
+{
+	
+}
+
+
+}
+
+//3
+int ahorcado(char adivinapalabra[E], char cadena[E])
+{
+}
+
+
+//4
+int imprime_ahorcado(int intentos)
+{
+}
+
+
