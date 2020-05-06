8b32a689f6dd6e606b5ba9471e6bfcedc9b0b0fb
davidbernaldo <d.bernaldo@alumnos.upm.es>
2020-05-06 09:59:19 +0200

    Rediseñado el sistema de combate

--- trabajo/Batalla/Simuladora.c
+++ trabajo/Batalla/Simuladora.c
@@ -1,46 +1,78 @@
 #include <stdio.h>
 
 
-
 int main (){
+	
+	// 0 - Sin tipo, 1 - Agua, 2 - Fuego, 3 - Planta
+	
+	//Ataque[3] = { Tipo, Daño, Precision}
+	
+	
 	//Estudiante
 	char nombre[9];
 	int vida_est;
-	int copiar[2] ={25, 33}, repasar[2] ={10, 100};
-	int eleccion;
+	int latigo_cepa[3] = {3, 25, 75}, pistola_agua[3] = {1, 15, 100}, ascuas[3] = {2, 35, 50}, cura[3] = {0, 0, 70};
+	int eleccion = 4;
 	
-	//Examen
-	int nota;
+	//Ash, tipo 2 - Fuego
 	
-	//Otros
+	
+	//Brock, tipo 3 - Planta
+	
+	
+	//Misty, tipo 1 - Agua
+	
+	
+	//Rival combate
+	int vida_rival = 200, tipo;
+	
+	//Otro
 	int turno = 1;
-	int tiempo;
+	int probabilidades;
 	
 	//Introduccion
-	printf("Bienvenido al simulador de examenes, por favor, escribe tu nombre: ");
+	printf("Bienvenido al simulador de combate, por favor, escribe tu nombre: ");
 	scanf("%s", &nombre);
 	
-	//Combate
-	while (nota < 100){
+	printf("A continuacion, %s elige a quien quieres enfrentarte: \n", nombre);
+	while(eleccion > 3){
+	printf("Ash <1> \nBrock <2> \nMisty <3>\n");
+	scanf("%i", &eleccion);
+		}
 		
-		//Contador
-		tiempo = 120 - turno * 20;
-		printf("\n\nQuedan %i minutos\n", tiempo);
+		
+	if (eleccion == 1)
+		tipo = 2;
+	else if (eleccion == 2)
+		tipo = 3;
+	else if (eleccion == 3)
+		tipo = 1;
+		
+	//Combate
+	while (vida_rival > 0){
+		
+		//Contador turnos
+		printf("\n\n\n-------------------------\nTurno %i\n\n", turno);
 		turno++;
 		
 		//Turno estudiante
-		printf("Elige una accion:\n Copiar<1> , Repasar<2>\n");
+		printf("Elige una accion:\n Latigo Cepa<1> , Pistola Agua<2>, Ascuas<3>, Cura<4>\n");
 		scanf("%i", &eleccion);
 		
 		if (eleccion == 1){
-				nota += copiar[0];
+				vida_rival -= latigo_cepa[1];
 			}
 		else if (eleccion == 2){
-				nota += repasar[0]; 
+				vida_rival -= pistola_agua[1]; 
 			}
-		printf("Tienes %i puntos\n\n", nota);
+		else if (eleccion == 3){
+				vida_rival -= ascuas[1];
+		}
+		else if (eleccion == 4){
+				vida_est += 25;
+		}
 		
 		
 	}
-printf("Felicidades %s has aprobado el examen", nombre);
+printf("Felicidades %s, has ganado", nombre);
 }
