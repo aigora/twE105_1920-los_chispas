edc7fa0ec3249f8b172da301884af8653814a55c
Enrique-MG <enrique.maseda.gonzalez@alumnos.upm.es>
2020-05-10 16:51:33 +0200

    Update 1.c
    Función para comprobar que el formato de casilla sigue el del tablero de juego, para luego seguir con la función de generación de barcos

--- trabajo/Hundir la flota/1.c
+++ trabajo/Hundir la flota/1.c
@@ -6,9 +6,13 @@
 #define nf 10
 #define nc 10
 
-int Printmapa1(int mat[nf][nc]); //Imprime mapa inicial con 0 en cada casilla
-int mapaIA[nf][nc];//matriz que representa los barcos del cpu
-int mapaAlmirante[nf][nc];//matriz que representa los barcos del jugador
+int Printmapa1(int mat[nf][nc]); //Imprime mapa inicial con ceros como agua en las casillas
+int mapaCPU[nf][nc]; //Mapa del ordenador
+int mapaAlmirante[nf][nc]; //Mapa del jugador
+int DisparosmapCPU[nf][nc]; //Mapa del rastro de disparos lanzados por la CPU
+int DisparosmapAlm[nf][nc]; //Mapa del rastro de disparos lanzados por el jugador
+int filmatriz, columatriz;
+char casilla[2]; //Casilla del tablero con formato letra + numero (ej: A1)
 
 
 void main()
@@ -21,15 +25,16 @@ void main()
 	for (i=0; i<nf; i++)
 		for (j=0; j<nc; j++)
 			{
-				mapaIA[i][j] = 0; 
+				mapaCPU[i][j] = 0; 
 				mapaAlmirante[i][j] = 0;
 			}	
 	Printmapa1(mapaAlmirante);
 	
+	
 
 }
 
-int Printmapa1(int mat[nf][nc]) //Imprime una matriz de tal forma que es legible al usuario y se muestra como un tablero
+int Printmapa1(int mat[nf][nc]) //Imprime una matriz como tablero inicial con casillas determinadas por una letra y un numero (ej: A1)
 {
     int i;
     
@@ -79,3 +84,45 @@ int Printmapa1(int mat[nf][nc]) //Imprime una matriz de tal forma que es legible
 		}  
 	    printf("\n---------------------------------------------\n");
 }
+int BarcosAlm(int mapaAlmirante[nf][nc]){
+	char casilla1, casilla2;
+	printf("Almirante decida el primer punto donde va a colocar su portaviones de 5 casillas");
+	scanf("%c", &casilla1);
+	printf("Almirante decida el último punto donde va a colocar su portaviones de 5 casillas");
+	scanf("%c", &casilla2);
+	
+}
+int infcasilla(char casilla[2]) //Comprueba si el formato de casilla introducio es válido
+{
+	if((casilla[0]>='a' && casilla[0]<='j') || (casilla[0]>='A' && casilla[0]<='J')){
+		if (casilla[0]=='a'||casilla[0]=='A') //asigna la letra introducida por el usuario a un numero (fila)
+				filmatriz = 0;
+		else if (casilla[0] == 'b' || casilla[0] == 'B')
+				filmatriz = 1;
+		else if (casilla[0] == 'c' || casilla[0] == 'C')
+				filmatriz = 2;
+		else if (casilla[0] == 'd' || casilla[0] == 'D')
+				filmatriz = 3;
+		else if (casilla[0] == 'e' || casilla[0] == 'E')
+				filmatriz = 4;
+		else if (casilla[0] == 'f' || casilla[0] == 'F')
+				filmatriz = 5;
+		else if (casilla[0] == 'g' || casilla[0] == 'G')
+				filmatriz = 6;
+		else if (casilla[0] == 'h' || casilla[0] == 'H')
+				filmatriz = 7;
+		else if (casilla[0] == 'i' || casilla[0] == 'I')
+				filmatriz = 8;
+		else if (casilla[0] == 'j' || casilla[0] == 'J')
+				filmatriz = 9;
+		else
+				return 0;
+	}
+	if ((casilla[1]-1) >= '0' && (casilla[1]-1) <= '9'){
+		columatriz = casilla[1]- '1';
+				return 1;
+	}
+		else
+				return 0;
+}
+
