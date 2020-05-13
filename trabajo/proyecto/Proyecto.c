c887597788bf449eb7320ab323f4a1412af44007
javierchaz234 <j.chazarrab@aumnos.upm.es>
2020-05-13 17:03:53 +0200

    Prueba de librerias

---
+++ trabajo/proyecto/Proyecto.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "ahorcadolib.h"
+
+void ahorcado();
+
+void main()
+{
+	char letra;
+  printf("Seleccione un juego a)Ahorcado\n");
+  scanf("%d", &letra);
+  if(letra=='a'|| letra == 'A')
+  ahorcado;
+  
+}
+
