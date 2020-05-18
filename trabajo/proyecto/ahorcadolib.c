dc4315597f9c36256d4adce2fa1c5c0f1a776c97
javierchaz234 <j.chazarrab@aumnos.upm.es>
2020-05-18 17:10:24 +0200

    librerias trabajo

--- trabajo/proyecto/ahorcadolib.c
+++ trabajo/proyecto/ahorcadolib.c
@@ -2,22 +2,14 @@
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
+
 #define N 10
 #define D 30
 #define F 20
 
 #include "ahorcadolib.h"
 
-//ESTRUCTURAS
-typedef struct{
-	char nomb[D];
-}adivinanza;
-
-typedef struct{
-	char nombre[D];
-	int punt;
-}usuario;
-
+/*
 //FUNCIONES 
 int numaleatorio(int n);//1
 void juego(char adivinapalabra[D], char cadena[D]);//2
@@ -26,7 +18,7 @@ int imp_ahorcado(int intentos);//4
 void Puntuaciones(usuario lista_punt[F],FILE *leer);//5
 
 //CODIGO PRINCIPAL
-/*void main()
+void main()
 {
     empezar:
 	system ("PAUSE");    	
@@ -86,8 +78,8 @@ void Puntuaciones(usuario lista_punt[F],FILE *leer);//5
 		goto empezar;
     }
 }
-
 */
+
 
 //FUNCIONES 
 //1
--- trabajo/proyecto/principal.c
+++ trabajo/proyecto/principal.c
@@ -2,7 +2,70 @@
 
 #include "ahorcadolib.h"
 
+void ahorcado_juego();
+
 void main()
 {
-	
+	ahorcado_juego();
+}
+
+
+void ahorcado_juego()
+{
+    do
+    {
+	system ("PAUSE");    	
+    system("cls");
+	char letra;
+	int intentos=0, fallos=0;
+	int aleatorio=0;
+	char letrausuario[D]=" ";
+	 adivinanza peliculas[8]={"MISION IMPOSIBLE", "TOP GUN", "STAR WARS", "INDIANA JONES", "JUMANJI", "LOS VENGADORES", "EL IRLANDES", "HARRY POTTER"};
+    adivinanza series[8]={"JUEGO DE TRONOS", "BREAKING BAD", "LA CASA DE PAPEL", "ELITE", "ARROW", "POKEMON", "VIKINGOS", "STRANGER THINGS"};
+	adivinanza teams[8]={"REAL MADRID", "ATLETICO DE MADRID", "BARCELONA", "LIVERPOOL", "WATFORD", "GENK", "RIVER", "BBOCA JUNIORS"};
+	printf("\tBienvenidos al Ahorcado !!!!!\n");
+		system ("PAUSE");
+			system("CLS");
+	printf("\tEstamos listos??\n");
+		system ("PAUSE");
+			system("CLS");
+	printf("Bien, comencemos!!!!\n");
+	printf("Selecciona categoria:\n\ta)Peliculas\n\tb)Series\n\tc)Equipos Futbol\n");
+   scanf(" %c", &letra);
+
+	switch (letra)
+    {
+       		case 'a':
+			case 'A':
+				system("CLS");
+				printf("Has elegido la categoria de Peliculas\n");
+				aleatorio = numaleatorio(8)-1;
+				juego(peliculas[aleatorio].nomb, letrausuario);
+	  			break;
+	           
+       		case 'b':
+			case 'B':
+				system("CLS");
+				printf("Has elegido la categoria de Series\n");
+				aleatorio = numaleatorio(8)-1;
+				juego(series[aleatorio].nomb, letrausuario);
+	  			break;
+	           
+       		case 'c':
+			case 'C':
+				system("CLS");
+				printf("Has elegido la categoria de Equipos de Futbol\n");
+				aleatorio = numaleatorio(8)-1;
+				juego(teams[aleatorio].nomb, letrausuario);
+	  			break;
+	           
+
+			default:
+			printf("Error, seleccione opcion valida\n");
+			system ("PAUSE");  
+			   			   			
+	}
+    	printf("\n Quieres jugar de nuevo? Pulsa 1:");
+
+		}while(getch()=='1');
 }
